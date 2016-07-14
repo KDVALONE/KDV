06e4aa9e7f8fa9a4d977bf0b223d0c6f185c783b
KDVALONE <KDV.LEARN.CSHARP@gmail.com>
2016-07-14 17:09:31 +0300

    Сделал установку UVNC и UVNC64 через функцию (CALL :INSTUVNC86, :INSTUVNC64)

--- BatScripts/UVNCINST.bat
+++ BatScripts/UVNCINST.bat
@@ -33,7 +33,7 @@ Echo advfirewall rule ICMPv4 %YN% >> LogBatIsntall.txt
 :: удаляем UVNC (Если до этого была установлена.) 
 
 ::принудительно завершить процесс wnvinc дождавшись выполенния (wait)
-Echo [%time:~,8%] stop task WINVNC
+Echo [%time:~,8%] kill task WINVNC
 start /wait taskkill /f /im winvnc*
 set e4=%ERRORLEVEL%
 IF %e4%==0 (CALL :OK) else ( CALL :NO)
@@ -65,9 +65,15 @@ Echo delete directory "uvncbvba" %YN% >> LogBatIsntall.txt
 :: запускаем установку UVNC в зависимости от архитектуры виндовс, в режиме пропуска подтверждения установки (/sp-)
 ::  с параметрами из лог файла, в скрытном режиме , по надобности с перезагрузкой без подтверждения /verysilent 
 echo [%time:~,8%] start install UVNC
-IF EXIST "%ProgramFiles(x86)%" ( echo [%time:~,8%] UVNC64 running intsall... 
-start /wait "" "%~d0%~p0distr\UVNC64.exe" /sp- /loadinf= "%~d0%~p0distr\vnclog.log" /verysilent ) ELSE ( echo [%time:~,8%] UVNC32 running install...
+IF EXIST "%ProgramFiles(x86)%" ( CALL :INSTUVNC86) else ( CALL :INSTUVNC64)
+:INSTUVNC86
+echo [%time:~,8%] UVNC64 running intsall... 
+start /wait "" "%~d0%~p0distr\UVNC64.exe" /sp- /loadinf= "%~d0%~p0distr\vnclog.log" /verysilent 
+GOTO :EOF
+:INSTUVNC64
+echo [%time:~,8%] UVNC32 running install...
 start /wait "" "%~d0%~p0distr\UVNC86.exe" /sp- /loadinf= "%~d0%~p0distr\vnclog.log" /verysilent )
+GOTO :EOF
 set e8=%ERRORLEVEL%
 IF %e8%==0 (CALL :OK) else ( CALL :NO)
 Echo start install UVNC %YN% >> LogBatIsntall.txt
