2a9dde6abb00c7fcbc50d68f384d7e9dd40ef203
KDVALONE <KDV.LEARN.CSHARP@gmail.com>
2016-07-18 15:48:49 +0300

    Добавил разрешение на удаленный доступ

--- BatScripts/UVNCINST.bat
+++ BatScripts/UVNCINST.bat
@@ -1,4 +1,4 @@
-﻿::v0.55  18.07.16
+﻿::v0.56  18.07.16
 :: для корректного отображения крилицы в CMD batch файл нужно сохранить в OEM 866
 @echo off
 cls
@@ -17,6 +17,15 @@ Set errorlevel=%ERRORLEVEL%
 IF errorlevel==0 ( Echo new pc name is %PCNAME% ) else ( Echo rename error )
 pause
 ::---------------------------------Rename PC cancel----------------------------------------------
+
+
+::---------------------------------Remote Desctop Access-----------------------------------------
+:::: разрешаем удаленный доступ к ПК, для разрешения правим реестр.
+Echo add Remote Desctop Access
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+Set errorlevel=%ERRORLEVEL%
+IF errorlevel==0 ( Echo OK ) else ( Echo rename error )
+::---------------------------------Remote Desctop Access cancel-----------------------------------
 
 
 ::---------------------------------add to Domain-------------------------------------------------
