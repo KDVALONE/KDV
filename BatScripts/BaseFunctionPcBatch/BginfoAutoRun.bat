98d33a43c85006cc7403df133430c182eda6bd58
KDVALONE <KDV.LEARN.CSHARP@gmail.com>
2016-07-21 13:21:09 +0300

    Поправил скрипт, сделав загрузку с обращением в лог

--- BatScripts/BaseFunctionPcBatch/BginfoAutoRun.bat
+++ BatScripts/BaseFunctionPcBatch/BginfoAutoRun.bat
@@ -7,13 +7,17 @@ md "%SystemDrive%\Program Files\BGInfo\"
 Echo copy distrib BGINFO
 copy "%~d0%~p0distr\BGInfo\" "%SystemDrive%\Program Files\BGInfo\" /y
 Echo BGinfo install
-start /wait "" "%SystemDrive%\Program Files\BGInfo\Bginfo.exe" /silent /timer:0
+set proga=%SystemDrive%\Program Files\BGInfo\Bginfo.exe
+set bgi=%SystemDrive%\Program Files\BGInfo\bginfo.bgi
+
+start /wait "" %proga% %bgi% /silent /timer:0
 
 set e=%ERRORLEVEL%
 IF %e%==0 ( Echo :OK ) else ( Echo NO )
 
-set proga=%SystemDrive%\Program Files\BGInfo\Bginfo.exe
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v BGINFO /t REG_SZ /d "%proga% /timer:0" /f
+
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v BGINFO /t REG_SZ /d "%proga% %bgi% /timer:0" /f
+
 set e=%ERRORLEVEL%
 IF %e%==0 ( Echo :OK ) else ( Echo NO )
 pause
