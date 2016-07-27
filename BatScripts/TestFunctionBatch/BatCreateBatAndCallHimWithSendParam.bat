85cb3367eaeb74a9e4b70e94e366ef92e324ba10
KDVALONE <KDV.LEARN.CSHARP@GMAIL.COM>
2016-07-28 00:38:05 +0400

    Добавил возможность в батник сосздания другого батника задавать вручную строки кода

--- BatScripts/TestFunctionBatch/BatCreateBatAndCallHimWithSendParam.bat
+++ BatScripts/TestFunctionBatch/BatCreateBatAndCallHimWithSendParam.bat
@@ -1,11 +1,16 @@
 
-:: באע פאיכ סמחהאוע באעפאיכ ג ןנמדנאללפאיכסו גûחûגגאוע ודמ
+::create new batch file with sended code custom variables  and call him, 
 @Echo off
 cls
+Echo add some string of code (example: Echo hello!)
+Set /p code=""
+
 Echo create new batch file
 pause
+
 Echo Echo off >> "%SystemDrive%\Program Files\CREATEDTESTBAT.bat"
 Echo cls >> "%SystemDrive%\Program Files\CREATEDTESTBAT.bat"
+Echo %code% >> "%SystemDrive%\Program Files\CREATEDTESTBAT.bat"
 Echo Echo CREATEDTESTBAT.bat will start success >> "%SystemDrive%\Program Files\CREATEDTESTBAT.bat"
 Echo pause >> "%SystemDrive%\Program Files\CREATEDTESTBAT.bat"
 Echo exit >> "%SystemDrive%\Program Files\CREATEDTESTBAT.bat"
