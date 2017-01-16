5cf7f8880c3538e0214c098dcd352614fba9203e
Dmitry <kdv.learn.csharp@gmail.com>
2017-01-16 22:05:51 +0300

    Ulearn CSHARP programm

---
+++ UlearnCSharpProgramms/TEST/Class/Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var city = new City();
+            city.Name = "Ekaterinburg";
+            city.Location = new GeoLocation();
+            city.Location.Latitude = 56.50;
+            city.Location.Longitude = 60.35;
+            Console.WriteLine("I love {0} located at ({1}, {2})",
+                city.Name,
+                city.Location.Longitude.ToString(CultureInfo.InvariantCulture),
+                city.Location.Latitude.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+   class City   { public string Name;     public GeoLocation Location;  }
+    class GeoLocation    {   public double Latitude = 0;     public double Longitude = 0;   }
+}
---
+++ UlearnCSharpProgramms/TEST/Test1/Test1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool enemyInFront = true;
+            string enemyName = "boss";
+            int robotHealth = 51;
+           
+           Console.WriteLine( ShouldFire2(enemyInFront, enemyName, robotHealth));
+            Console.ReadLine(); 
+        }
+
+            static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            //   return enemyInFront && ((enemyName == "boss") && (robotHealth > 100));
+            bool shouldFire = true;
+            if (enemyInFront == true)
+            {
+                if (enemyName == "boss")
+                {
+                    if (robotHealth < 50) shouldFire = false;
+                    if (robotHealth > 100) shouldFire = true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return shouldFire;
+
+        }
+    }
+}
+
