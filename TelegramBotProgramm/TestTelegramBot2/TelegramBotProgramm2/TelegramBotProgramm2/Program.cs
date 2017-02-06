fb3a83c716090d6aa22b73097a58007c7cdd0b1b
Dmitry <kdv.learn.csharp@gmail.com>
2017-02-06 13:57:41 +0300

    Написал часть текста проги

--- TelegramBotProgramm/TestTelegramBot2/TelegramBotProgramm2/TelegramBotProgramm2/Program.cs
+++ TelegramBotProgramm/TestTelegramBot2/TelegramBotProgramm2/TelegramBotProgramm2/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using 
+
 
 namespace TelegramBotProgramm2
 {
@@ -10,6 +13,30 @@ namespace TelegramBotProgramm2
     {
         static void Main(string[] args)
         {
+        }
+
+        public delegate void Response(object sender, ParameterResponse e);
+        public class ParameterResponse : EventArgs
+        {
+            public string name;
+            public string message;
+            public string chatId;
+        }
+
+        public class TelegramRequest
+        {   
+            // класс обработчик запросов
+            public string Token;
+            int LastUpdateId = 0;
+
+            public event Response ResponseRecived; //событие, при получение ответа
+            ParameterResponse e = new ParameterResponse();
+
+            public void GetUpdate()
+            {
+                while(true)
+                    string
+            }
         }
     }
 }
