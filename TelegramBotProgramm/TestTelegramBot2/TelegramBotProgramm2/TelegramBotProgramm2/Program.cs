22996e6884ebd6ddfea3975580edf22ecdfdd48b
KDVALONE <kdv.learn.csharp@gmail.com>
2017-02-09 14:16:45 +0300

    Задал пространство имен Telegram  в файле PerformCommand

--- TelegramBotProgramm/TestTelegramBot2/TelegramBotProgramm2/TelegramBotProgramm2/Program.cs
+++ TelegramBotProgramm/TestTelegramBot2/TelegramBotProgramm2/TelegramBotProgramm2/Program.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Threading;
+using Telegram;
+using Telegram.Reqest;
 
 namespace TelegramBotProgramm2
 {
     class Program
     {
-        public string Token = "249206099:AAHaLl2nxqwdmRR0mQZWJW5fsYLxL_mJnPU";
+       // public string Token = "249206099:AAHaLl2nxqwdmRR0mQZWJW5fsYLxL_mJnPU";
         static void Main(string[] args)
         {
             TelegramRequest Tr = new TelegramRequest();
@@ -22,11 +24,14 @@ namespace TelegramBotProgramm2
             {
                 Console.WriteLine("Фоновый поток работает");
                 Thread.Sleep(500);
-
             }
             
         }
 
-    
+        private static void Tr_ResponseRecived(object sender, ParameterResponse e)
+        {
+            Console.WriteLine("{0}: {1}  chatId:{2} ", e.name, e.message, e.chatId);
+        }
+        }
     }
-}
+
