21c65214c95a71894d1e35862dd0f58b0392b4a2
Dmitry <kdv.learn.csharp@gmail.com>
2017-02-16 15:42:06 +0300

    пишу подключение к тестовой БД.

---
+++ SqlProgramm/TelegramBotProgrammSQLtest/TelegramBotProgrammSQLtest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+
+namespace TelegramBotProgrammSQLtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;";
+            //"Server=127.0.0.1;" + "Database=glpi_db;" + "Uid=root;" + "Pwd=;"
+            //"server=192.168.104.12;user=tech1;database=tpc2;port=3306;password=Y-96mnrSw;Allow Zero Datetime=true";
+            //"server=localhost;user=root;database=glpi_db;port=3306;password=;";
+            MySqlConnection sqlConnection = new MySqlConnection(connectionString);
+            try
+            { Console.WriteLine("Connect to DB.."); sqlConnection.Open(); }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+            Console.WriteLine("Close connect...");
+            sqlConnection.Close();
+            
+
+        }
+    }
+}
