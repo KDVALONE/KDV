dfe4cfde6b9f58dfef4221d24d2638c59aee8e87
Dmitry <kdv.learn.csharp@gmail.com>
2017-03-14 00:02:15 +0300

    1

---
+++ HeadFirstCSharpProgramms/Chapter_5/Chapter_5_Programm_1(inkaps)/Chapter_5_Programm_1(inkaps)/DinnerParty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_5_Programm_1_inkaps_
+{
+    class DinnerParty
+    {
+        const int CostOfFoodPerPerson = 25;
+        public int NuumberOfPeople;
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations = 0;
+
+        public void SetHealthyOption() {}
+        public void CalculateCostOfDecorations() { }
+        public decimal CalculateCost() { }
+
+
+
+    }
+}
