8cbb83d7a0fd1f88de473fffddb5e9a18c39a56e
Dmitry <kdv.learn.csharp@gmail.com>
2017-08-02 09:59:24 +0300

    Делаю тестовые задачи

---
+++ TroelsenCSharpProgramms/Chapter6-inheritance-polymorphism/IsAndAsTest/Сonversion/IsAndAsTest/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAndAsTest
+{
+    abstract class Human
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private string Sex { get; set; }
+        private DateTime BDate { get; set; }
+        
+
+        public Human(string[] sex = { "Men","Woomen"}; )
+        {
+
+        int[] a = { 1, 2, 3, 4, 5 };
+        Console.WriteLine (a[new Random().Next(0, a.Length)]);
+
+          
+        } 
+      
+    }
+}
---
+++ TroelsenCSharpProgramms/Chapter6-inheritance-polymorphism/IsAndAsTest/Сonversion/IsAndAsTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAndAsTest
+{
+    class Program
+    {
+        /// <summary>
+        /// Создать 5 классов  Человек -- Специалист -- Главный Дизайнр -- Дизайнер
+        ///                                          -- программист
+        ///  отработать наследование,
+        ///  полиморфизм,
+        ///  приведение явное и неявное.
+        ///  обращение к базовым классам с помощью конструктора и тд.
+        ///  
+        /// </summary>
+        /// <param name="args"></param>
+        /// 
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
