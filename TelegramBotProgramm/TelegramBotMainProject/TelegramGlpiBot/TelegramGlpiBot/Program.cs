e9d28f44dfa3e1c4f7be9db68f7a5be370bfa725
Dmitry <kdv.learn.csharp@gmail.com>
2017-10-19 21:54:14 +0300

    (ВАЖНО)v0.0.0.1 -  Создал сборку для продакшена GLPITelegramBot, сюда пишем основного бота. Не тест.

---
+++ TelegramBotProgramm/TelegramBotMainProject/TelegramGlpiBot/TelegramGlpiBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramGlpiBot
+{
+    class Program
+    {
+        /// <summary>
+        /// Основной класс программы
+        /// Версия 0.0.0.1
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
