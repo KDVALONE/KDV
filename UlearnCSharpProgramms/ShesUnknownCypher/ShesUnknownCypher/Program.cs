3df9955e318ae956a6272c4bc940c2f4d0b0b852
KDVALONE <kdv.learn.csharp@gmail.com>
2017-10-19 20:58:30 +0300

    Добавил решение (не доделано) шифра незнакомки.Ulearn.

---
+++ UlearnCSharpProgramms/ShesUnknownCypher/ShesUnknownCypher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShesUnknownCypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "решИла нЕ Упрощать и зашифРОВАтЬ Все послаНИЕ " 
+                          +"дАже не Старайся нИЧЕГО у тЕбя нЕ получится с расшифРОВкой "
+                          +"Сдавайся НЕ твоего ума Ты не споСОбЕн Но может быть "
+                          +"если особенно упорно подойдешь к делу "
+                          +"будет Трудно конечнО "
+                          +"Код ведЬ не из простых "
+                          +"очень ХОРОШИЙ код "
+                          +"то у тебя все получится "
+                          +"и я буДу Писать тЕбЕ еще "
+                          +"чао";
+
+            List<string> textList = new List<string>();
+            textList.AddRange(text.Split(' ')); 
+            textList.Reverse();
+            List<string> rezult = new List<string>();
+            foreach (string element in textList)
+                {
+                    if (Char.IsUpper(element[0]))
+                        rezult.Add(element);
+                }
+            Console.WriteLine(string.Join(" ", rezult.ToArray()));
+            Console.ReadKey();
+        }
+
+        //private static string DecodeMessage(string[] lines)
+        //{
+
+        //    return cypher;
+        //}
+
+    }
+}
