74059030ede86d543d140392cb7a1d48e32824ea
Dmitry <kdv.learn.csharp@gmail.com>
2018-06-21 18:18:01 +0300

    Распределил все по папкам

--- GameDevelopment/TravelerGame/InterfaceProgramm/StaticClassFolder/CityPlaceGenerator.cs
+++ GameDevelopment/TravelerGame/InterfaceProgramm/StaticClassFolder/CityPlaceGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace TravelerGame
 {/// <summary>
@@ -11,16 +12,26 @@ namespace TravelerGame
     static class CityPlaceGenerator
     {
 
-        public static List<CityPlace> GetCityPlaceList(CityTypes CytyPlace)
+        public static List<CityPlace> GetCityPlaceList(CityTypes cityType)
         {
-
             List<CityPlace> cityPlaceList = new List<CityPlace>();
+            Type ourtype = typeof(CityPlace);
+            Type[] types = Assembly.GetAssembly(ourtype).GetTypes().Where(type => type.IsSubclassOf(ourtype)).ToArray();
+            for (int i = 0; i < types.Length; i++)
+            {
+                CityPlace cityPlace = (CityPlace)Activator.CreateInstance(types[(types.Length)]);
+                cityPlaceList.Add(cityPlace);
+            }
+            
 
             return cityPlaceList;
         }
 
         private static CityPlace AddCityPlace()
         {
+            Random rnd = new Random();
+            
+
             return CityPlace
         }
 
---
+++ SomeTestPrgrmFolder/TestProgrammToTrening/TestReflection/SetListOfDerrivedClass/SetListOfDerClas/SetListOfDerClas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+namespace SetListOfDerClas
+{
+    /// <summary>
+    /// Рефлексия. заполнить список наследниками абюрстрактного класса, причем у нектрых нет конструкторов по умолчанию.
+    /// 
+    /// </summary>
+    abstract class Room { abstract public void Print(); }
+
+    public enum SomeEnum { one = 1, two }
+    class Kitchen : Room { override public void Print() { Console.WriteLine("Kitchen"); } }
+    class Bedroom : Room { override public void Print() { Console.WriteLine("Bedroom"); } }
+    class Hall : Room
+    {
+        SomeEnum CountIteration { get; set; }
+        public Hall(SomeEnum countIteration)
+        {
+            CountIteration = countIteration;
+        }
+        override public void Print() { Console.WriteLine("Hall"); }
+        public void SomeMet() { Console.WriteLine($"Что-то  делать {CountIteration} раза"); }
+
+        class Program
+        {
+            static List<Room> rooms;
+            public static Random rnd = new Random();
+            static void Main()
+            {
+
+                rooms = new List<Room>();
+                Type ourtype = typeof(Room);
+                Type[] types = Assembly.GetAssembly(ourtype).GetTypes().Where(type => type.IsSubclassOf(ourtype)).ToArray();
+                for (int i = 0; i < rnd.Next(100); i++)
+                {
+                    SomeEnum e = SomeEnum.two;
+                    int element = rnd.Next(types.Length);
+                    Room room;
+                    if (types[element] == typeof(Hall))
+                    { room = (Room)Activator.CreateInstance(types[element], e); }
+                    else { room = (Room)Activator.CreateInstance(types[element]); }
+                    rooms.Add(room);
+                }
+                foreach (Room r in rooms)
+                {
+                    if (r is Hall)
+                    { (r as Hall).SomeMet(); }
+                    else { r.Print(); }
+
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
+
