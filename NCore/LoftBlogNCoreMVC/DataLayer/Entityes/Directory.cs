7b154391aad2aec03b0bc7fd3c116473e505b5c4
KDVALONE <kdv.learn.csharp@gmail.com>
2019-10-12 02:30:38 +0300

    Выполнил Урок3. Создание классов сущностей для оперирования в коде, подключение EFCORE, подключили CodFirst подход засчет миграций, создание БД

---
+++ NCore/LoftBlogNCoreMVC/DataLayer/Entityes/Directory.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace DataLayer.Entityes
+{
+    /// Создали слой L3 - DataLayer, слой в даном случае это библиотека работы с БД
+    /// В папке Entityes сущности для работы с БД
+  
+    /// <summary>
+    ///Directory содержит список Materials
+    /// </summary>
+    public class Directory : Page
+    {
+        public List<Material> Materials { get; set; }
+    }
+}
---
+++ NCore/LoftBlogNCoreMVC/DataLayer/Entityes/Page.cs
@@ -0,0 +1,15 @@
+﻿namespace DataLayer.Entityes
+{
+    /// Создали слой L3 - DataLayer, слой в даном случае это библиотека работы с БД
+    /// В папке Entityes сущности для работы с БД
+
+    /// <summary>
+    /// Верхнеуровневый уровень сущности, от него наследуются Directory и Materials
+    /// </summary>
+    public class Page
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Html { get; set; }
+    }
+}
