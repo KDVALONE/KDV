c2d980742ae7d2e3d7e84a6564ece7dad46dfef0
KDVALONE <kdv.learn.csharp@gmail.com>
2019-10-15 06:49:21 +0300

    Урок5 WebHost...

--- NCore/LoftBlogNCoreMVC/LoftBlogNCoreMVC/Program.cs
+++ NCore/LoftBlogNCoreMVC/LoftBlogNCoreMVC/Program.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore;
+using DataLayer;
+using Microsoft.AspNetCore.Http;
 
 namespace LoftBlogNCoreMVC
 {
@@ -13,21 +17,29 @@ namespace LoftBlogNCoreMVC
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-           var host = CreateWebHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-           {
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
 
-           }
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
 
-
+                var context = services.GetRequiredService<EFDBContext>();
+                SampleData.InitData(context);
+            }
+            //public static IHostBuilder CreateHostBuilder(string[] args) =>
+            //Host.CreateDefaultBuilder(args)
+            //    .ConfigureWebHostDefaults(webBuilder =>
+            //    {
+            //        webBuilder.UseStartup<Startup>();
+            //    });
+            host.RunAsync();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                 webBuilder.UseStartup<Startup>();
+            });
     }
 }
