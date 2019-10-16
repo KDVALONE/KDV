c0d480e1cd585b82071c9db8a0368c36ec6c7823
KDVALONE <kdv.learn.csharp@gmail.com>
2019-10-16 04:15:54 +0300

    Урок6 Использование DataLayer с Entitiye framework с Data Layer, через конироолер. Строгосвязанный подход

--- NCore/LoftBlogNCoreMVC/LoftBlogNCoreMVC/Controllers/HomeController.cs
+++ NCore/LoftBlogNCoreMVC/LoftBlogNCoreMVC/Controllers/HomeController.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LoftBlogNCoreMVC.Models;
+using DataLayer;
+using DataLayer.Entityes;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoftBlogNCoreMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private EFDBContext _context ;
+        public HomeController(EFDBContext context)
+        {
+            _context = context;
+        }
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -21,7 +29,8 @@ namespace LoftBlogNCoreMVC.Controllers
         public IActionResult Index()
         {
             HelloModel _model = new HelloModel() {HelloMessage = "Az Buki Vedi"};
-            return View(_model);
+            List<Directory> _dirs = _context.Directory.Include(x =>x.Materials).ToList();
+            return View(_dirs);
         }
 
         public IActionResult Privacy()
