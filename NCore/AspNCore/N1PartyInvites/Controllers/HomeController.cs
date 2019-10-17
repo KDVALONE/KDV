3720a476ac4ea67ae413a49ecd898395cbff4a55
KDVALONE <kdv.learn.csharp@gmail.com>
2019-10-17 03:05:03 +0300

    Добавил базовый проект NetCore по книге Фриман

---
+++ NCore/AspNCore/N1PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using N1PartyInvites.Models;
+
+namespace N1PartyInvites.Controllers
+{
+    public class HomeController : Controller    
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+    }
+}
