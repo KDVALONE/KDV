ecf40e44def534b96441c016d6e8a029f4b0b355
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-10 19:14:05 +0300

    Фримен. AspNetCore. Глава 4. Использовани Null условной операции

--- NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Controllers/HomeController.cs
+++ NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using  Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using N2LanguageFeatures.Models;
 namespace N2LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            return View(new string[] {"C#", "Language", "Features"});
+            List<string> result = new List<string>();
+            foreach (Product p in Product.GetProducts() )
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                result.Add(string.Format("Name:{0}, Price:{1}", name, price));
+            }
+
+            return View(result);
         }
     }
 }
