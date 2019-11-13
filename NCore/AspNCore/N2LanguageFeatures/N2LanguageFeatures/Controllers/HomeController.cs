1cb3d609b2176eab5200b173655348e53b4b5ba7
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-13 19:20:07 +0300

    Фримен. AspNetCore. Глава 4. Инициализация коллекции с помощью Dictionary

--- NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Controllers/HomeController.cs
+++ NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Controllers/HomeController.cs
@@ -7,16 +7,31 @@ namespace N2LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            List<string> result = new List<string>();
-            foreach (Product p in Product.GetProducts() )
+            Dictionary<string, Product> products = new Dictionary<string, Product> // инициализация коллекции с помощью словаря
             {
-                string name = p?.Name ?? "No Name";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name ?? "<None>";
-                result.Add(string.Format($"Name:{name}, Price:{price},Related:{relatedName}"));
-            }
+                // старый синтаксис
+                //{"Kayak",new Product{ Name = "Kayak", Price = 275M }  },
+                //{"Lifejacket", new Product{ Name = "Lifejacket", Price = 48.95M} } 
 
-            return View(result);
+                //более простой новый синтаксис инициализация коллекции с помощью словаря
+                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+            };
+
+            return View("Index", products.Keys);
+
+            // return View("Index", new string[] {"Bob", "Joe", "Alice"}); //использование инициализации коллекции
+
+            //List<string> result = new List<string>();
+            //foreach (Product p in Product.GetProducts() )
+            //{
+            //    string name = p?.Name ?? "No Name";
+            //    decimal? price = p?.Price ?? 0;
+            //    string relatedName = p?.Related?.Name ?? "<None>";
+            //    result.Add(string.Format($"Name:{name}, Price:{price},Related:{relatedName}"));
+            //}
+
+            //return View(result);
         }
     }
 }
