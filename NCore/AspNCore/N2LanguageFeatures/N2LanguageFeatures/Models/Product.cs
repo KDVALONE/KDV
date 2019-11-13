702163da713b8087ef5be1b4443afad945b0b7ab
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-13 18:24:22 +0300

    Фримен. AspNetCore. Глава 4. Автосовойства (ничего нового, просто обычные авто свойства =))

--- NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Models/Product.cs
+++ NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Models/Product.cs
@@ -1,18 +1,26 @@
-﻿namespace N2LanguageFeatures.Models
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace N2LanguageFeatures.Models
 {
     public class Product
     {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; }
         public static Product[] GetProducts()
         {
-            Product kayak = new Product {Name = "Kayak", Price = 275M};
-            Product lifejacet = new Product {Name = "Lifejacket", Price = 48.95M};
+            Product kayak = new Product {Name = "Kayak", Category = "Water Craft",Price = 275M};
+            Product lifejacet = new Product(false) {Name = "Lifejacket", Price = 48.95M};
 
             kayak.Related = lifejacet;
 
-            return new Product[] {kayak, lifejacet, null};
+            return new Product[] { kayak, lifejacet, null };
         }
     }
 }
