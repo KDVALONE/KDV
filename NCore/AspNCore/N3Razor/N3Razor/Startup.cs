5cd0e951b22d0697ef9516eb869729f7d60baab0
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-19 01:05:46 +0300

    Фримен. AspNetCore. Глава 5. N3Razor. Добавил MVC в пустой шаблон.

--- NCore/AspNCore/N3Razor/N3Razor/Startup.cs
+++ NCore/AspNCore/N3Razor/N3Razor/Startup.cs
@@ -8,13 +8,16 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace N3Razor
-{
+{/// <summary>
+/// Фримен. AspNetCore. Глава 5. N3Razor. Приложение для тренировки работы с RAZOR.
+/// </summary>
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,11 @@ namespace N3Razor
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
