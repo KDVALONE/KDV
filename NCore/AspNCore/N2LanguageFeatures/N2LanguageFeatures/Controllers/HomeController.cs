47d0f688512b26a4c97859dbf25930a8ae7c0752
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-18 18:00:04 +0300

    Фримен. AspNetCore. Глава 4. использовние выражения nameof для облегчения обращения к свойствам, и предотвращения ошибок на этапе выполнения.

--- NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Controllers/HomeController.cs
+++ NCore/AspNCore/N2LanguageFeatures/N2LanguageFeatures/Controllers/HomeController.cs
@@ -3,80 +3,101 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using N2LanguageFeatures.Models;
+using System;
+using  System.Linq;
+
 namespace N2LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
+        //public async Task<ViewResult> Index()
+        //{
         #region синтаксис с использованием асинхронных методов действия
-        public async Task<ViewResult> Index()
+        //    long? length = await MyAsyncMethods.GetPageLength();
+        //    return View(new string[] {$"Length:{length}"});
+        #endregion
+        //}
+
+        public ViewResult Index()
         {
-            long? length = await MyAsyncMethods.GetPageLength();
-            return View(new string[] {$"Length:{length}"});
-        }
-        #endregion
-        //public ViewResult Index()
-        //{
+            #region синтаксис с использованием выражения nameof
+            //данный синтаксис облегчает отлов ошибок по сравнению с обычным LINQ
+            //и исключает возникновение ошибок на этапе выполнения, свзанных с неверным строгим заданным именем полей.
+            //IntelliSense при использовании nameof выдает больше подсказок, а ошибки возникают только на этапе компиляции,
+            // что не дает им ускользнуть от внимания разраб-ка.
 
-        #region синтаксис с использованием методов расширения с лямбда выражением 
-        //позволяет не писать кучу методов расширения для новых филтраций, а делать это быстро и лаконично с помощью лямбд
-        //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
-        //Product[] productsArray =
-        //{
-        //    new Product { Name = "Kayak", Price = 275M},
-        //    new Product { Name = "Lifejacket", Price = 48.95M},
-        //    new Product { Name = "Soccer ball", Price = 19.50M},
-        //    new Product { Name = "Corner flag", Price = 34.95M}
-        //};
-        //decimal priceFilterTotal = productsArray
-        //    .Filter(p => (p?.Price ?? 00) >= 20).TotalPrices();
-        //decimal nameFilterTotal = productsArray
-        //    .Filter(p => p?.Name?[0] == 'S').TotalPrices();
-        //return View("Index", new string[]
-        //{
-        //    $"Price Total: {priceFilterTotal:С2}",
-        //    $"Name Total: {nameFilterTotal:C2}"
-        //});
-        #endregion
+            var products = new []
+            {
+                new Product { Name = "Kayak", Price = 275M},
+                new Product { Name = "Lifejacket", Price = 48.95M},
+                new Product { Name = "Soccer ball", Price = 19.50M},
+                new Product { Name = "Corner flag", Price = 34.95M}
+            };
+            return View(products.Select(p =>
+                $"{nameof(p.Name)}:{p.Name},{nameof(p.Price)}:{p.Price}"));
 
-        #region синтаксис с использованием методов расширения
-        //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            #endregion
 
-        //decimal cartTotal = cart.TotalPrices(); // применение метода расширения TotalPrice класса MyExtensionMethods расширяющего ShoppingCart
-        //return View("Index", new string[] {$"Total: {cartTotal:C2}"});
+            #region синтаксис с использованием методов расширения с лямбда выражением 
+            //позволяет не писать кучу методов расширения для новых филтраций, а делать это быстро и лаконично с помощью лямбд
+            //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            //Product[] productsArray =
+            //{
+            //    new Product { Name = "Kayak", Price = 275M},
+            //    new Product { Name = "Lifejacket", Price = 48.95M},
+            //    new Product { Name = "Soccer ball", Price = 19.50M},
+            //    new Product { Name = "Corner flag", Price = 34.95M}
+            //};
+            //decimal priceFilterTotal = productsArray
+            //    .Filter(p => (p?.Price ?? 00) >= 20).TotalPrices();
+            //decimal nameFilterTotal = productsArray
+            //    .Filter(p => p?.Name?[0] == 'S').TotalPrices();
+            //return View("Index", new string[]
+            //{
+            //    $"Price Total: {priceFilterTotal:С2}",
+            //    $"Name Total: {nameFilterTotal:C2}"
+            //});
+            #endregion
 
-        #endregion
+            #region синтаксис с использованием методов расширения
+            //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
 
-        #region синтаксис с использованием методов расширения применительно к интерфейсу
-        //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
-        //Product[] productsArray =
-        //{
-        //    new Product { Name = "Kayak", Price = 275M},
-        //    new Product { Name = "Lifejacket", Price = 48.95M},
-        //    new Product { Name = "Soccer ball", Price = 19.50M},
-        //    new Product { Name = "Corner flag", Price = 34.95M}
+            //decimal cartTotal = cart.TotalPrices(); // применение метода расширения TotalPrice класса MyExtensionMethods расширяющего ShoppingCart
+            //return View("Index", new string[] {$"Total: {cartTotal:C2}"});
 
-        //};
-        //синтаксис для использования мет.расширения к интерфейсам БЕЗ ФИЛЬТРАЦИИ.
-        //decimal cartTotal = cart.TotalPrices();
-        //decimal arrayTotal = productsArray.TotalPrices();
+            #endregion
 
-        //return View("Index", new string[]
-        //{
-        //    $"Cart Total: {cartTotal:C2}",
-        //    $"Array Total: {arrayTotal:C2}"
-        //});
+            #region синтаксис с использованием методов расширения применительно к интерфейсу
+            //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            //Product[] productsArray =
+            //{
+            //    new Product { Name = "Kayak", Price = 275M},
+            //    new Product { Name = "Lifejacket", Price = 48.95M},
+            //    new Product { Name = "Soccer ball", Price = 19.50M},
+            //    new Product { Name = "Corner flag", Price = 34.95M}
 
-        //*********************
-        //синтаксис для использования мет.расширения к интерфейсам C ФИЛЬТРАЦИЕЙ.
-        //decimal arrayTotal = productsArray.FilterByPrice(20).TotalPrices();
-        //return View("Index", new string[] {$"Array Total: {arrayTotal:C2}"});
+            //};
+            //синтаксис для использования мет.расширения к интерфейсам БЕЗ ФИЛЬТРАЦИИ.
+            //decimal cartTotal = cart.TotalPrices();
+            //decimal arrayTotal = productsArray.TotalPrices();
 
-        #endregion
+            //return View("Index", new string[]
+            //{
+            //    $"Cart Total: {cartTotal:C2}",
+            //    $"Array Total: {arrayTotal:C2}"
+            //});
 
-        #region синтаксис с использованием IS и SWITCH - сопоставление с образцом
+            //*********************
+            //синтаксис для использования мет.расширения к интерфейсам C ФИЛЬТРАЦИЕЙ.
+            //decimal arrayTotal = productsArray.FilterByPrice(20).TotalPrices();
+            //return View("Index", new string[] {$"Array Total: {arrayTotal:C2}"});
+
+            #endregion
+
+            #region синтаксис с использованием IS и SWITCH - сопоставление с образцом
 
 
-        object[] data = new object[] {275M, 29.95M, "apple", "orange",100, 10 };
+            object[] data = new object[] {275M, 29.95M, "apple", "orange",100, 10 };
             decimal total = 0;
             //синтаксис с использованием IS
             //for (int i = 0; i < data.Length; i++)
@@ -142,6 +163,6 @@ namespace N2LanguageFeatures.Controllers
             //return View(result);
 
             #endregion
-        //}
+        }
     }
 }
