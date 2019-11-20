d967862f70cfe8de3ebc19ca730a199245b8863a
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-20 03:53:26 +0300

    Фримен. AspNetCore. Глава 6. N4WorkingWithVisualStudio. Приложение для отработки функционала VS с ASP.NetCore. - подключение MVC к пустому Razor шаблону, создание модели, контроллера, представления и временного репозитория для представления

---
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using N4WorkingWithVisualStudio.Models;
+
+namespace N4WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
+    }
+}
