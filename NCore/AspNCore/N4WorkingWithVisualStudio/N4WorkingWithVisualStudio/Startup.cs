5dded2e9aba0fb4058d005b8be814eedb5c80f49
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-26 18:27:13 +0300

    Фримен. AspNetCore. Глава 7. N4WorkingWithVisualStudio. Создание формы ввода данных AddProduct, подключение к закрытой папке node_module для обращение к файлам стилей. Применение стилек к страницам приложения

--- NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio/Startup.cs
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace N4WorkingWithVisualStudio
 {
@@ -27,6 +29,15 @@ namespace N4WorkingWithVisualStudio
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+            //подключение к закрытой папке node_module куда npm какчает bootstrap, обязятельно должна быть после app.UseStaticFiles()
+            app.UseFileServer(new FileServerOptions()  
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, @"node_modules") //строка получения пути к папке
+                ),
+                RequestPath = "/node_modules", //это вроде псевдоним для обращения.
+                EnableDirectoryBrowsing = false
+            });
         }
     }
 }
---
+++ NCore/AspNCore/TestBootstrapAdd/TestBootstrapAdd/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+
+namespace TestBootstrapAdd
+{/// <summary>
+/// Проверяю устновку NPM и применение файлов из закрытой директории node_module
+///ссылка https://wildermuth.com/2017/11/19/ASP-NET-Core-2-0-and-the-End-of-Bower
+/// 
+/// Способ 1. Установить необходимый путь к директории используюя app.UserFileServer.
+/// https://metanit.com/sharp/aspnet5/13.1.php
+/// Но есть 2 косяка: 1 - путь в тегах все равно отмечается как не найденный но это не так.
+///                   2 - сама идеология обращения к node_module плохой подход, так как все должно быть только в wwwroot/css или wwwroot/css
+///                     так же могут возникнуть (читал на форумах) проблемы с отображением у пользователей в каких то случаях.
+/// (ПРАВИЛЬНЫЙ) Способ 2. С помощью GULP перенаправлять файлы в каталоги в wwwroot/css или wwwroot/css
+/// https://metanit.com/sharp/aspnet5/13.1.php
+/// </summary>
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            
+              // подключаем файлы по умолчанию
+              app.UseDefaultFiles();
+
+              #region Способ 1 - подключение к закрытой директории node_module 
+
+              // подключаем статические файлы
+              app.UseStaticFiles();
+
+              // добавляем поддержку каталога node_modules !!! Обязательно после app.UseStaticFiles();
+              app.UseFileServer(new FileServerOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(
+                        Path.Combine(env.ContentRootPath, @"node_modules") //строка получения пути к папке
+                    ),
+                    RequestPath = "/node_modules", //это вроде псевдоним для обращения.
+                    EnableDirectoryBrowsing = false
+                });
+              #endregion
+        }
+    }
+}
