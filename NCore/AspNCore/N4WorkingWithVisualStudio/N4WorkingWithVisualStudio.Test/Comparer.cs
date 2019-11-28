bfae412970e0a885ee0234be379817bc9b778a2e
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-28 17:04:40 +0300

    Фримен. AspNetCore. Глава 7. N4WorkingWithVisualStudio. Добавил файл Comparer.cs к тестовому решению,  содерж. классы  для возможности применения лямбда выражений в модульных тестах, это не обязательно, но упростит написание тестов.

--- NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/Comparer.cs
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/Comparer.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 namespace N4WorkingWithVisualStudio.Test
 {
+    /// <summary>
+    /// В данных классах содержится код для возможности применения лямбда выражений в модульных тестакх
+    /// это не обязательно, но упростит написание тестов.
+    /// TODO: (Разобрать добавление лямбда выражений в модульные тесты) 
+    /// </summary>
+    
     public class Comparer
     {
         public static Comparer<U> Get<U>(Func<U, U, bool> func)
@@ -10,6 +16,23 @@ namespace N4WorkingWithVisualStudio.Test
             return new Comparer<U>(func);
         }
     }
+    public class Comparer<T> : Comparer, IEqualityComparer<T>
+    {
+        private Func<T, T, bool> comparisonFunction;
+        public Comparer(Func<T, T, bool> func)
+        {
+            comparisonFunction = func;
+        }
 
- 
+        public bool Equals(T x, T y)
+        {
+            return comparisonFunction(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
 }
