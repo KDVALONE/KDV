e3d62b2b90d376824d7a96bd137409823f0c8d6c
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-28 18:27:06 +0300

    Фримен. AspNetCore. Глава 7. N4WorkingWithVisualStudio. Написал класс для тестирования боле сложных классов содержащих зависимости. Метод IndexActionModelComplete()  проверяет что метод действия Index() передает представлению все обьекты хранилища. Должен выдовать ошибку, так как обьекты возвр. Index() отличаются от ожидаемых

---
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using N4WorkingWithVisualStudio.Controllers;
+using N4WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace N4WorkingWithVisualStudio.Test
+{
+    /// <summary>
+    /// класс для тестирования боле сложных классов содержащих зависимости
+    /// </summary>
+    public class HomeControllerTest
+    {
+        // тест проверяет что метод действия Index() передает представлению все обьекты хранилища
+        [Fact]
+        public void IndexActionModelComplete()
+        {
+            //организация
+            var controller = new HomeController();
+            //действие
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            //утвреждение
+            Assert.Equal(SimpleRepository.SharedRepository.Products, model,
+                        Comparer.Get<Product>((p1,p2)=>p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+    }
+}
