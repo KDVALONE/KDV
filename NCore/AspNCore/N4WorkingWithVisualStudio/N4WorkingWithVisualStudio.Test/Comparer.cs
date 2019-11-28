4cd534bb5972c221c6e65f5cd90ed9305307c7e6
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-28 16:59:46 +0300

    Фримен. AspNetCore. Глава 7. N4WorkingWithVisualStudio. Добавил проект новое решение для тестов, добавил класс для теста класса Product, написал проверяющие тесты

---
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/Comparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace N4WorkingWithVisualStudio.Test
+{
+    public class Comparer
+    {
+        public static Comparer<U> Get<U>(Func<U, U, bool> func)
+        {
+            return new Comparer<U>(func);
+        }
+    }
+
+ 
+}
---
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/ProductTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using N4WorkingWithVisualStudio.Models;
+
+namespace N4WorkingWithVisualStudio.Test
+{
+    /// <summary>
+    /// Класс тестирующий класс Product
+    /// Составлен по шаблону А/А/А  arrange/act/assert
+    /// Имя класса говорит что он тестирует
+    /// Имена методов говорят какое поведение или состояние они проверяют
+    /// </summary>
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            // организация arrange, в него входят условия теста
+            var p = new Product { Name = "Test", Price = 100M};
+            // действие act, выполнение теста
+            p.Name = "New Name";
+            // утверждение assert, ожидаемый результат
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            //Орагнизация
+            var p = new Product {Name = "Test", Price = 100M};
+            //Действие
+            p.Price = 200M;
+            //Утверждение
+            Assert.Equal(200M, p.Price);
+        }
+
+    }
+}
