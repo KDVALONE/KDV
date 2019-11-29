e42096e7aae9a2d79d4ef8a23c9b3c0fd939ee37
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-29 11:13:25 +0300

    Фримен. AspNetCore. Глава 7. N4WorkingWithVisualStudio. Для проведения корректного тестирования реализовал изоляцию HomeController путем реализации и применения интерфейса IRepository для согласованности данных, тест все еще не проходит но уже более явно указывает на проблему в методе действия Index(), при осмотре можно определить что проблема с методом Where в LINQ. Но для явного нахождения проблемы нужно еще дополнительно улучшить тест.

--- NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/HomeControllerTest.cs
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/HomeControllerTest.cs
@@ -11,16 +11,32 @@ namespace N4WorkingWithVisualStudio.Test
     /// </summary>
     public class HomeControllerTest
     {
+        class ModelCompleteFakeRepository : IRepository
+        {
+            public IEnumerable<Product> Products { get; } = new Product[]
+            {
+                new Product {Name = "P1", Price = 275M},
+                new Product {Name = "P2", Price = 48.95M},
+                new Product {Name = "P3", Price = 19.50M},
+                new Product {Name = "P3", Price = 34.95M}
+            };
+
+            public void AddProduct(Product p)
+            {
+                //ничего не делать - для теста не требуется
+            }
+        }
         // тест проверяет что метод действия Index() передает представлению все обьекты хранилища
         [Fact]
         public void IndexActionModelComplete()
         {
             //организация
             var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepository();
             //действие
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
             //утвреждение
-            Assert.Equal(SimpleRepository.SharedRepository.Products, model,
+            Assert.Equal(controller.Repository.Products, model,
                         Comparer.Get<Product>((p1,p2)=>p1.Name == p2.Name && p1.Price == p2.Price));
         }
     }
