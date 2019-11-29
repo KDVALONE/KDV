0eedcaa892c19d6511a4faa8a6a7eeabe261e583
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-29 16:08:12 +0300

    Фримен. AspNetCore. Глава 7. N4WorkingWithVisualStudio. Создание иммитационных mock тестов. Подключил в NUGET расширение moq для написания иммитационных тестов. С помощью них переписал фиктивыне реализации хранилищ, упростив код. (стр203)

--- NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/HomeControllerTest.cs
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/HomeControllerTest.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using N4WorkingWithVisualStudio.Controllers;
 using N4WorkingWithVisualStudio.Models;
 using Xunit;
+using Moq;
 
 namespace N4WorkingWithVisualStudio.Test
 {
@@ -12,29 +13,19 @@ namespace N4WorkingWithVisualStudio.Test
     /// </summary>
     public class HomeControllerTest
     {
-        // фиктивоное хранилище данных для теста
-        class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<Product> Products { get; set; }
-
-            public void AddProduct(Product p)
-            {
-                //ничего не делать - для теста не требуется
-            }
-        }
-
-        // тест проверяет что метод действия Index() передает представлению все обьекты хранилища, с применением прамаетрезации.
+      // тест проверяет что метод действия Index() передает представлению все обьекты хранилища, с применением прамаетрезации.
         [Theory]
         [ClassData(typeof(ProductTestData))] // данный атрибут конфигуриуется с типом тестовых данных (ProductTestData), во время выполнения создасться
         //экземпляр ProductTestData и будет применен для получения последовательных тестовых данных
         public void IndexActionModelIsComplete(Product [] products)
         {
             //организация
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository
-            { 
-                Products = products
-            };
+
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+
+            var controller = new HomeController {Repository = mock.Object};
+
             //действие
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
             //утвреждение
@@ -42,30 +33,7 @@ namespace N4WorkingWithVisualStudio.Test
                 Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
 
-        /// <summary>
-        /// Класс для фиктивной реализации классов
-        /// В данном случае тест выполняет просто подсчет обращений к Products, фиктивного хранилища
-        /// и проверяет точно ли обращались всего один раз
-        /// </summary>
-        class PropertyOnceFakeRepository : IRepository
-        {
-            public int PropertyCounter { get; set; } = 0;
-
-            public IEnumerable<Product> Products
-            {
-                get
-                {
-                    PropertyCounter++;
-                    return new[] {new Product {Name = "P1", Price = 100}};
-                }
-            }
-
-            public void AddProduct(Product p)
-            {
-                //ничего не делать - для теста не требуется
-            }
-        }
-        /// <summary>
+   /// <summary>
         /// тест выполняет просто подсчет обращений к Products, фиктивного хранилища
         /// и проверяет точно ли обращались всего один раз
         /// </summary>
@@ -73,14 +41,17 @@ namespace N4WorkingWithVisualStudio.Test
         public void RepositoryPropertyCalledOnce()
         {
             //организация arrange
-            var repo = new PropertyOnceFakeRepository();
-            var controller = new HomeController {Repository = repo};
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products)
+                .Returns(new[] {new Product {Name = "P1", Price = 100}});
+
+            var controller = new HomeController {Repository = mock.Object};
 
             //действие act
             var result = controller.Index();
 
             //утверждение assert
-            Assert.Equal(1,repo.PropertyCounter);
+            mock.VerifyGet(m=>m.Products,Times.Once);
         }
     }
 }
