a1ac4d4910cc5b5cf561a1fecfaf05359c037cfc
KDVALONE <kdv.learn.csharp@gmail.com>
2019-11-29 12:23:12 +0300

    Фримен. AspNetCore. Глава 7. N4WorkingWithVisualStudio. Подготовил новый тест IndexActionModelIsCompletePriceUnder50() с применением нового хранилища, содержавшем елементы только меньше 50

--- NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/HomeControllerTest.cs
+++ NCore/AspNCore/N4WorkingWithVisualStudio/N4WorkingWithVisualStudio.Test/HomeControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using N4WorkingWithVisualStudio.Controllers;
 using N4WorkingWithVisualStudio.Models;
@@ -38,6 +39,40 @@ namespace N4WorkingWithVisualStudio.Test
             //утвреждение
             Assert.Equal(controller.Repository.Products, model,
                         Comparer.Get<Product>((p1,p2)=>p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+        /// <summary>
+        /// Тест с применением нового хранилища, содержавшем елементы только меньше 50
+        /// </summary>
+        class ModelCompleteFakeRepositoryPricesUnder50 : IRepository
+        {
+            public IEnumerable<Product> Products { get; } = new Product[]
+            {
+                new Product{ Name = "P1", Price = 5M}, //вот тут заменили с 275 на 5
+                new Product{ Name = "P2", Price = 48.95M},
+                new Product{ Name = "P3", Price = 19.50M}, 
+                new Product{ Name = "P4", Price = 34.95M}, 
+            };
+
+            public void AddProduct(Product p)
+            {
+                //ничего не делать - для теста не требуется
+            }
+        }
+
+        [Fact]
+        public void IndexActionModelIsCompletePriceUnder50()
+        {
+            //организация
+            var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
+
+            //действие
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            //утвреждение
+            Assert.Equal(controller.Repository.Products, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
     }
 }
