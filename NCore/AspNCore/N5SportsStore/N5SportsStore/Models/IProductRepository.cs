de7a4250886df267bac42e7d55ec1f5836f5e62f
KDVALONE <kdv.learn.csharp@gmail.com>
2019-12-03 19:47:23 +0300

    Фримен. AspNetCore. Глава 8. N5SportsStore. Создал интерфейс IRepsitory : IQueryable для предоставления только необходимых данных из БД, создал фейковое временное хранилище данных FakeProductRepository : IRepository

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace N5SportsStore.Models
+{
+    /// <summary>
+    /// Класс зависящий от IProductRepository способен получать обьекты Product без необходимости знания детелей того,
+    /// как они хранятся. К тому же при запросах LINQ использование данного и. даст возможность получать только те данные,
+    /// которые удовлетворяют запросу, а не все, с дальнейшей выборкой, что снизит нагрузку на БД.
+    /// </summary>
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
