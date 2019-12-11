fc6c7b37c65c0f66d554ebd049301bd97ca82703
KDVALONE <kdv.learn.csharp@gmail.com>
2019-12-12 01:24:15 +0300

    Фримен. AspNetCore. Глава 8. N5SportsStore. Создал файл хранилища получающий от EF данные - EFProductRepository (будет расширяться в дальнейшем), создал строку подключения к ДБ добавив в проект "файл параметров приложения"  appsettings.json (стр224)

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace N5SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
