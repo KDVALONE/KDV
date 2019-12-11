89ba940021f8767632ae9311eda557661f927768
KDVALONE <kdv.learn.csharp@gmail.com>
2019-12-12 00:08:37 +0300

    Фримен. AspNetCore. Глава 8. N5SportsStore. Добавил конектс данных - класс ApplicationDbContext, служащий прослойкой между моделью и ДБ в EFCore

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace N5SportsStore.Models
+{
+    /// <summary>
+    /// Контекст данных служит прослойкой для взаимодействия модели и ДБ в EFCore
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base()
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
