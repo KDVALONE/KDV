ee805e5c78d86f068bc02445f5bfc2f7a7eb31ae
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-09 14:27:03 +0300

    Фримен. AspNetCore. Глава 8. N5SportsStore. Создал модульный тест для проверки функционала разделения на страницы.

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using N5SportsStore.Controllers;
+using N5SportsStore.Models;
+using Xunit;
+
+namespace N5SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            //Организация
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            
+            //Действие
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+            
+            //Утверждение
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length ==2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
