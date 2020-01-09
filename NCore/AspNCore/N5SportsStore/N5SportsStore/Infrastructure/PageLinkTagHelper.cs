7eaab4fb4aa088fbab43415ae410e80f485ad9c9
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-09 17:07:40 +0300

    Фримен. AspNetCore. Глава 8. N5SportsStore. Создал класс вспомогательной функции дескриптора PagingLinkTagHelper который заполняет div елементами a , которые соответствуют страницам товаров. Также зарегистрировал его в _ViewImports

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using N5SportsStore.Models.ViewModels;
+
+namespace N5SportsStore.Infrastructure
+{
+    //вообще в папку VM добавляются классы которые предост. прил. связующий код, но не имеют к отношения к предметной области приложения.
+    /// <summary>
+    /// Класс заполняет div елементами a, которые соответствуют страницам товаров.
+    /// </summary>
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction,
+                    new {productPage = i});
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+
+    }
+}
