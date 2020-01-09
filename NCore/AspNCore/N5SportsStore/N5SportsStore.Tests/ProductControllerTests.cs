67cd2d2140eb1660ff229303892af332ac6e481f
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-09 21:40:40 +0300

    Фримен. AspNetCore. Глава 8. N5SportsStore. Создал модульный тест Can_Send_Pagination_View_Model  для проверки что контроллер предоставляет корректные данные представлению для разбиения на страницы, так же немного изменил предидущий тест контроллера в связи с изменением кода.

--- NCore/AspNCore/N5SportsStore/N5SportsStore.Tests/ProductControllerTests.cs
+++ NCore/AspNCore/N5SportsStore/N5SportsStore.Tests/ProductControllerTests.cs
@@ -3,12 +3,16 @@ using System.Linq;
 using Moq;
 using N5SportsStore.Controllers;
 using N5SportsStore.Models;
+using N5SportsStore.Models.ViewModels;
 using Xunit;
 
 namespace N5SportsStore.Tests
 {
     public class ProductControllerTests
     {
+        /// <summary>
+        /// Модульный тест создания ссылкок на страницы
+        /// </summary>
         [Fact]
         public void Can_Paginate()
         {
@@ -24,15 +28,46 @@ namespace N5SportsStore.Tests
             }).AsQueryable<Product>());
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
-            
+
             //Действие
-            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+            // IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+            ProductsListViewModel result =
+                controller.List(2).ViewData.Model as ProductsListViewModel;
             
             //Утверждение
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length ==2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        /// <summary>
+        /// Модульный тест данные для разбиения страницы для модели представления
+        /// </summary>
+        [Fact]
+        public void Can_Send_Pagination_View_Model() 
+        {
+            //Организация
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+            new Product { ProductID = 1, Name = "P1" },
+            new Product { ProductID= 2, Name = "P2" },
+            new Product { ProductID = 3, Name = "P3" },
+            new Product { ProductID= 4, Name = "P4" },
+            new Product { ProductID= 5, Name = "P5" }
+            }).AsQueryable<Product>());
+            
+            //Организация
+            ProductController controller =  new ProductController(mock.Object) { PageSize = 3};
+            //Действие
+            ProductsListViewModel result = controller.List(2).ViewData.Model as ProductsListViewModel;
+            //Утверждение
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+
         }
     }
 }
