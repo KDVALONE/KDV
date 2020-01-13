caecc2c66d3f356b5157a2dae76deccd63a6465b
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-13 21:21:39 +0300

    Фримен. AspNetCore. Глава 9. N5SportsStore. Исправил отображение счетчика страниц  товаров определенной категории (раньше он отображал страницы даже без содержания, теперь он зависит от кол-ва товаров), написал тест для данного счетчика.

--- NCore/AspNCore/N5SportsStore/N5SportsStore.Tests/ProductControllerTests.cs
+++ NCore/AspNCore/N5SportsStore/N5SportsStore.Tests/ProductControllerTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using N5SportsStore.Controllers;
 using N5SportsStore.Models;
@@ -32,11 +34,11 @@ namespace N5SportsStore.Tests
             //Действие
             // IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
             ProductsListViewModel result =
-                controller.List(null,2).ViewData.Model as ProductsListViewModel;
-            
+                controller.List(null, 2).ViewData.Model as ProductsListViewModel;
+
             //Утверждение
             Product[] prodArray = result.Products.ToArray();
-            Assert.True(prodArray.Length ==2);
+            Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
         }
@@ -45,7 +47,7 @@ namespace N5SportsStore.Tests
         /// Модульный тест данные для разбиения страницы для модели представления
         /// </summary>
         [Fact]
-        public void Can_Send_Pagination_View_Model() 
+        public void Can_Send_Pagination_View_Model()
         {
             //Организация
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
@@ -56,11 +58,11 @@ namespace N5SportsStore.Tests
             new Product { ProductID= 4, Name = "P4" },
             new Product { ProductID= 5, Name = "P5" }
             }).AsQueryable<Product>());
-            
+
             //Организация
-            ProductController controller =  new ProductController(mock.Object) { PageSize = 3};
+            ProductController controller = new ProductController(mock.Object) { PageSize = 3 };
             //Действие
-            ProductsListViewModel result = controller.List(null,2).ViewData.Model as ProductsListViewModel;
+            ProductsListViewModel result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
             //Утверждение
             PagingInfo pageInfo = result.PagingInfo;
             Assert.Equal(2, pageInfo.CurrentPage);
@@ -72,7 +74,8 @@ namespace N5SportsStore.Tests
 
         ///модульный тест проверки функциональности фильтрации по категориям
         [Fact]
-        public void Can_Filter_Products() {
+        public void Can_Filter_Products()
+        {
 
             //Организация- создание имитированного хранилища
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
@@ -96,6 +99,41 @@ namespace N5SportsStore.Tests
             Assert.Equal(2, result.Length);
             Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
+
+        ///модульный тест для проверки счетчика товаров определенной категории
+        [Fact]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //Организация
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1,  Name = "P1",  Category = "Cat1" },
+                new Product { ProductID = 2,  Name = "P2" , Category = "Cat2" },
+                new Product { ProductID = 3,  Name = "P3",  Category = "Cat1" },
+                new Product { ProductID = 4,  Name = "P4" , Category = "Cat2" },
+                new Product { ProductID = 5,  Name = "P5" , Category = "Cat3" }
+
+            }).AsQueryable<Product>());
+
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            Func<ViewResult, ProductsListViewModel> GetModel = result =>
+            result?.ViewData?.Model as ProductsListViewModel;
+
+            //Действие
+            int? res1 = GetModel(target.List("Cat1"))?.PagingInfo.TotalItems;
+            int? res2 = GetModel(target.List("Cat2"))?.PagingInfo.TotalItems;
+            int? res3 = GetModel(target.List("Cat3"))?.PagingInfo.TotalItems;
+            int? resAll = GetModel(target.List(null))?.PagingInfo.TotalItems;
+
+            //Утверждение
+            Assert.Equal(2, res1);
+            Assert.Equal(2, res2);
+            Assert.Equal(1, res3);
+            Assert.Equal(5, resAll);
         }
     }
 }
