ea664cec961047e8beb81912ee7a3dda2372af9f
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-14 00:05:44 +0300

    Фримен. AspNetCore. Глава 9. N5SportsStore. Создал класс Cart  использующий класс CartLine представляющий товар выбранный пользователем, Он содержит методы для добавления , удаления, вычисления общей стоимости элементов корзины и метод очистки корзины, путем удаления всех элем.

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Models/Cart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace N5SportsStore.Models
+{   /// <summary>
+    ///  класс Cart использует класс CartLine представляющий товар выбранный пользователем, Он содержит методы для добавления элем. в корзину,
+    ///  удаления, вычисления общей стоимости элементов корзины и метод очистки корзины, путем удаления всех элем.
+    /// </summary>
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(p => p.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+            if (line == null) { 
+            lineCollection.Add(new CartLine
+                { 
+                    Product = product,
+                    Quantity = quantity
+                });
+            } else {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) =>
+            lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        public virtual decimal ComputeTotalValue() =>
+            lineCollection.Sum(e => e.Product.Price * e.Quantity);
+        public virtual void Clear() => lineCollection.Clear();
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
+    }
+    public class CartLine {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
