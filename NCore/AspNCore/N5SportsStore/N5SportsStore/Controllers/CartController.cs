e08e414ed05f416d1e08b9befd24fb04f324cb11
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-14 18:53:55 +0300

    Фримен. AspNetCore. Глава 9. N5SportsStore. Добавил контроллер для реализации взаимодействия с корзиной и ее механикой, добавил файл расширения для сериализации состояния хранения корзины в формате JSON

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using N5SportsStore.Infrastructure;
+using N5SportsStore.Models;
+
+namespace N5SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId,string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            if(product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJon<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+
+        }
+    }
+}
