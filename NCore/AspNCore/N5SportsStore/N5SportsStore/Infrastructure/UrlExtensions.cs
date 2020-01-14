d7e62d453b6d5d930c6bd73afe0f6de15c30d711
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-14 16:34:24 +0300

    Фримен. AspNetCore. Глава 9. N5SportsStore. Функционал  кнопки добавления в корзину, путем добавления метода расширения PathAndQuery() класса UrlExtensions для работы с HTTP ответами, добавил пространста имен Infrastructure в файл _ViewImports, так же добавил саму кнопкув файле ProductSummary

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace N5SportsStore.Infrastructure
+{
+    /// <summary>
+    /// класс расширяющих методов для кнопок
+    /// </summary>
+    public static class UrlExtensions
+    {
+        /// <summary>
+        /// Метод расширения для класса HttpRequest для генерации URL по которому браузер будет возвращаться после обновления корзины
+        /// принимая во внимание строку запроса, если она есть.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static string PathAndQuery(this HttpRequest request) =>
+            request.QueryString.HasValue
+                ? $"{request.Path}{request.QueryString}"
+                : request.Path.ToString();
+    }
+}
