775bcd5fb36cd6d4417f0235bedbede042f0f678
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-21 00:07:06 +0300

    Фримен. AspNetCore. Глава 10. N5SportsStore. Первый этап функционала создания заказов, создал модель Order.cs, представление Checkout.cshtml и контроллер OrderController.cs, подробнее о проверки достоверности и привязки моделей (binding) и их атрибутах в классе модели Order.cs на стр 290 и главах 27 и 26

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using N5SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace N5SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
