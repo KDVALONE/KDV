979d118db5e978532943038c349801d83be3c392
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-22 02:17:40 +0300

    Фримен. AspNetCore. Глава 11. Применил атрибуты проверки достоверности к модели Product

--- NCore/AspNCore/N5SportsStore/N5SportsStore/Models/Product.cs
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Models/Product.cs
@@ -1,10 +1,17 @@
-﻿namespace N5SportsStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace N5SportsStore.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage ="Please enter a product name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+        [Required]
+        [Range(0.01,double.MaxValue,ErrorMessage ="Please specify a category")]
         public decimal Price { get; set; }
         public string Category { get; set; }
         
