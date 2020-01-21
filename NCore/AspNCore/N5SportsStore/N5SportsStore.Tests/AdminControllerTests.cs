e950a2ed2652f8602577d3b62ade13595c440018
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-21 22:14:28 +0300

    Фримен. AspNetCore. Глава 11. Начал реализовывать управление каталогом товаров, написал контроллер Администратора AdminController и модульный тест к нему.

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Linq;
+using Xunit;
+using N5SportsStore.Controllers;
+using N5SportsStore.Models;
+namespace N5SportsStore.Tests
+{
+   public class AdminControllerTests
+    {
+
+        /// <summary>
+        /// Проверка корректного возвращения обьектов PRODUCT контроллером AdminController
+        /// </summary>
+        [Fact]
+        public void Index_Contains_All_Products()
+        {
+            //Организация - создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+                {
+                    new Product{ ProductID = 1, Name= "P1"},
+                    new Product{ ProductID = 2, Name= "P2"},
+                    new Product{ ProductID = 3, Name= "P3"},
+                }).AsQueryable<Product>());
+            //Организация - создание контроллера
+            AdminController target = new AdminController(mock.Object);
+            //Действие
+            Product[] result
+                = GetViewModel<IEnumerable<Product>>(target.Index())?.ToArray();
+
+        }
+
+        private T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using N5SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace N5SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index() => View(repository.Products);
+    }
+}
