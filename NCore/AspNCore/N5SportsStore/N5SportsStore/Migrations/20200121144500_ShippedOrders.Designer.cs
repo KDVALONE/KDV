6db29aed453892c0e6dccbcf3b58d55f12cb2d1b
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-21 18:35:13 +0300

    Фримен. AspNetCore. Глава 11. N5SportsStore Добавляю функционала работы с заказами, Расширил модель Order, сделал миграцию для Order, добавил методы действия в контроллер OrderController, добавил представление List в Views/Order, добавил компановку представления _AdminLayout

--- NCore/AspNCore/N5SportsStore/N5SportsStore/Migrations/20200121144500_ShippedOrders.Designer.cs
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Migrations/20200121144500_ShippedOrders.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using N5SportsStore.Models;
 
 namespace N5SportsStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200121144500_ShippedOrders")]
+    partial class ShippedOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
