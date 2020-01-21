152bfa2c61da09ae875338353267035ac03a9a6f
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-21 18:39:05 +0300

    Фримен. AspNetCore. Глава 11. [OFF TOP] промежуточный коммит,

--- NCore/AspNCore/N5SportsStore/N5SportsStore/Startup.cs
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Startup.cs
@@ -15,7 +15,7 @@ using Microsoft.Extensions.FileProviders;
 namespace N5SportsStore
 {
     /// <summary>
-    /// Фримен. AspNetCore. Глава 11. N5SportsStore Администрирование. Максимально приближенный к реальности проект NetCore MVC на NCore 2.1
+    /// Фримен. AspNetCore. Глава 11. N5SportsStore Администрирование.Максимально приближенный к реальности проект NetCore MVC на NCore 2.1
     /// </summary>
     public class Startup
     {
