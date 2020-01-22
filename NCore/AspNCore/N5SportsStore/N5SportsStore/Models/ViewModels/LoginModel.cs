6414fcd05061981dc6386a6080c15bcab510fad0
KDVALONE <kdv.learn.csharp@gmail.com>
2020-01-22 22:28:04 +0300

    Фримен. AspNetCore. Глава 12. N5SportsStore  Создал контроллер да Account и предствалений к нему.

---
+++ NCore/AspNCore/N5SportsStore/N5SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace N5SportsStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        //подробнее стр 338 и глава 24
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+
+    }
+}
