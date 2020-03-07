3e799939ede39d5f88e6aa8e3ee209fa64b5c2b1
KDVALONE <kdv.learn.csharp@gmail.com>
2020-03-07 15:05:44 +0300

    Созадал WPF калькулятор, просто вспоминал синтаксис. Создал WinService  Службу мониторинга измения папки с логированием. По метаниту.

---
+++ SomeTestPrgrmFolder/WinServices/MonitoringFolderSevice/MonitoringFolderSevice/Service1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonitoringFolderSevice
+{
+    /// <summary>
+    /// Служба для мониторинга изменения в папке файлов. С метанита
+    /// </summary>
+
+    public partial class Service1 : ServiceBase
+    {
+        Logger logger; //TODO: Декомпазировать класс Logger  и переименовать его.
+        public Service1()
+        {
+            InitializeComponent();
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            logger = new Logger();
+
+            //TODO: Может на TASK, вообще почитать чем там TASK от прямого THREAD отличается
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            logger.Stop();
+            Thread.Sleep(1000);
+        }
+    }
+    class Logger
+    {
+        FileSystemWatcher watcher;
+        object obj = new object(); // для LOCK
+        bool enabled = true;
+        public Logger()
+        {
+            watcher = new FileSystemWatcher("F:\\Temp");
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Created += Watcher_Created;
+            watcher.Changed += Watcher_Changed;
+            watcher.Renamed += Watcher_Renamed;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+            //TODO: Возможно либо удалить либо переписать на событийную модель, к примеру сделать 
+            // enable  observable collection <bool> ну или тип того
+            while (enabled)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+            enabled = false;
+        }
+
+        // переименование файлов
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            string fileEvent = "переименован в " + e.FullPath;
+            string filePath = e.OldFullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+        // изменение файлов
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "изменен";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+        // создание файлов
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "создан";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+        // удаление файлов
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "удален";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void RecordEntry(string fileEvent, string filePath)
+        {
+            // TODO: Разобрать lock.  Чтобы не было гонки ресурсов за файл templog.txt, 
+            // в который вносятся записи об изменениях, процедура записи блокируется заглушкой lock(obj).
+            lock (obj)
+            {
+                using (StreamWriter writer = new StreamWriter("F:\\templog.txt", true))
+                {
+                    writer.WriteLine(String.Format("{0} файл {1} был {2}",
+                           DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+                    writer.Flush();
+                }
+            }
+        }
+    }
+}
